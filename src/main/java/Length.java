e8a53f47ecc11d70724922687068067c56b98fc3
梦鸿 <1016279039@qq.com>
2022-05-23 20:32:22 +0800

    feat:最终款

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -1,46 +1,56 @@
 public class Length {
     private final double value;
-    private final String unit;
+    private final Unit unit;
 
-    public Length(double val, String uinnt) {
-        this.value = val;
-        this.unit = uinnt;
+    public Length(double value, Unit unit) {
+        this.value = value;
+        this.unit = unit;
     }
 
-    public Length as(String u) {
-        Length len = this;
-        if (this.unit.equals("f")) {
-            if (u.equals("yard")) {
-                len = new Length(this.value / 3, u);
-            } else if (u.equals("inch")) {
-                len = new Length(this.value * 12, u);
-            }
-        }
-
-        if (this.unit.equals("yard")) {
-            if (u.equals("inch")) {
-                len = new Length(this.value * 36, u);
-            } else if (u.equals("f")){
-                len = new Length(this.value * 3, u);
-            }
-        }
-
-        if (this.unit.equals("inch")) {
-            if (u.equals("f")) {
-                len = new Length(this.value / 12, u);
-            } else if (u.equals("yard")) {
-                len = new Length(this.value / 36, u);
-            }
-        }
-
-        return len;
+    /**
+     * 想要用unit替换String
+     * @return
+     */
+    public Length as(Unit targetUnit) {
+        double value = newValueIn(targetUnit);
+        return new Length(value, targetUnit);
     }
 
-    public double getVal() {
+    private double newValueIn(Unit targetUnit) {
+        if (this.unit == Unit.FOOT) {
+            if (targetUnit == Unit.YARD) {
+                return this.value /3;
+            }
+            if (targetUnit == Unit.INCH) {
+                return this.value * 12;
+            }
+        }
+
+        if (this.unit == Unit.YARD) {
+            if (targetUnit == Unit.INCH) {
+                return this.value * 36;
+            }
+            if (targetUnit == Unit.FOOT){
+                return this.value *3;
+            }
+        }
+
+        if (this.unit == Unit.INCH) {
+            if (targetUnit == Unit.FOOT) {
+                return this.value / 12;
+            }
+            if (targetUnit == Unit.YARD) {
+                return this.value /36;
+            }
+        }
         return this.value;
     }
 
-    public String getUinnt() {
-        return this.unit;
+    public double getValue() {
+        return value;
+    }
+
+    public Unit getUnit() {
+        return unit;
     }
 }
